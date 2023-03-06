1f0453eab35faa332c0c6f77879284d315643feb
piwlak <adriamorales@gmail.com>
2023-03-06 11:20:05 -0600

    delete

--- lib/screen/settings_page.dart
+++ lib/screen/settings_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:moviles1/common/local_storage.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../provider/theme_provider.dart';
